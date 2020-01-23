bd99155640ef1de7886e962a157d73c91c91734f
Mitch Gates <Mitch.Gates@veritas.com>
2020-01-22 21:48:21 -0600

    Fixed errors in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 with open("requirements.txt") as f:
-    install_requires = f.splitlines()
+    install_requires = f.readlines()
 
 setuptools.setup(
     name='ipmon',
@@ -15,7 +15,7 @@ setuptools.setup(
     license='MIT',
     author='Mitch Gates',
     author_email='gates55434@gmail.com',
-    url='https://github.com/mistergates/ipmon'
+    url='https://github.com/mistergates/ipmon',
     packages=setuptools.find_packages(),
     install_requires=install_requires,
     python_requires='>=3.4',
@@ -23,6 +23,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
+    ]
 )
  
